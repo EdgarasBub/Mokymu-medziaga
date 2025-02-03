25e6667c43e78dc85e97dea55d5ec5cba19f3fee
EdgarasBub <e.bubulis@gmail.com>
2025-02-03 16:58:06 +0200

    kepykla

--- 7-js-dom/2-uzduotys/kepykla/sripts.js
+++ 7-js-dom/2-uzduotys/kepykla/sripts.js
@@ -12,10 +12,65 @@ calculateButton.addEventListener('click', function () {
     // console.log('day-Reservations', dayReservations);
 
     let bakeryTotal = employeesCount * employeesmakesPerDay;
+    let isBakeryGonnaMakeIt = bakeryTotal >= dayReservations;
 
+    // let logInfo ={
+    //     bakeryTotal,
+    //     isBakeryGonnaMakeIt
 
-    let logInfo ={
-        bakeryTotal
-    };
-    console.log(logInfo);
+    // };
+    // console.log(logInfo);
+
+    let resultsDiv = document.getElementById('results');
+    // console.log(resultsDiv);
+    resultsDiv.innerHTML = `<p><strong>kepykla per diena spes pagaminti: </strong>${bakeryTotal} kepalikuu</p>`;
+    resultsDiv.innerHTML += `<p><strong> Jai reikia speti pagaminti: </strong>${dayReservations}Kepaliuku</p>`
+    resultsDiv.innerHTML+= `<p><strong>Ar spes pagaminti?</strong>${isBakeryGonnaMakeIt ? 'Taip' : 'Ne'}</p>`
+})
+
+document.getElementById('employees-count').addEventListener('keyup', function (event) {
+    // console.log('reiksme pasikeite');
+    let inputValue = event.target.valueAsNumber;
+
+    if(inputValue < 0 ) {
+        // console.log(event.target);
+        event.target.classList.add('error');
+        event.target.nextElementSibling.classList.add('show')
+    } else {
+        event.target.classList.remove('error');
+        event.target.nextElementSibling.classList.remove('show')
+    }
+})
+document.getElementById('employees-makes-per-day').addEventListener('keyup', function (event) {
+    // console.log('reiksme pasikeite');
+    let inputValue = event.target.valueAsNumber;
+
+    if(inputValue < 0 ) {
+        // console.log(event.target);
+        event.target.classList.add('error');
+        event.target.nextElementSibling.classList.add('show')
+    } else {
+        event.target.classList.remove('error');
+        event.target.nextElementSibling.classList.remove('show')
+    }
+})
+document.getElementById('day-reservations').addEventListener('keyup', function (event) {
+    // console.log('reiksme pasikeite');
+    let inputValue = event.target.valueAsNumber;
+
+    if(inputValue < 0 ) {
+        // console.log(event.target);
+        event.target.classList.add('error');
+        event.target.nextElementSibling.classList.add('show')
+    } else {
+        event.target.classList.remove('error');
+        event.target.nextElementSibling.classList.remove('show')
+    }
+});
+document.getElementById('reset').addEventListener('click', function(){
+    // console.log('clicked')
+    document.getElementById('employees-count').value = 0;
+    document.getElementById('employees-makes-per-day').value = 0;
+    document.getElementById('day-reservations').value = 0;
+    document.getElementById('results').innerHTML = '<p>Kol kas nieko nera </p>'
 })
