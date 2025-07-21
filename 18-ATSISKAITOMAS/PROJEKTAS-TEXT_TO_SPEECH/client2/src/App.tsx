fc7c2265f9fcae99efaa99043f99c6f8c3fd1563
EdgarasBub <edgaras.bub@gmail.com>
2025-07-21 18:10:09 +0300

    atsiskaitomo papildymas

--- 18-ATSISKAITOMAS/PROJEKTAS-TEXT_TO_SPEECH/client2/src/App.tsx
+++ 18-ATSISKAITOMAS/PROJEKTAS-TEXT_TO_SPEECH/client2/src/App.tsx
@@ -1,4 +1,6 @@
 import React from 'react';
+import {BrowserRouter, Route, Routes} from 'react-router-dom'
+
 import TextToSpeech from './pages/TextToSpeech';
 import Navbar from './components/NavBar'
 import Home from './pages/Home';
@@ -6,7 +8,11 @@ import Home from './pages/Home';
 const App: React.FC = () => {
   return (
     <main className="bg-gray-100 min-h-screen p-4">
-      <Navbar />
+      <BrowserRouter>
+            <Navbar />
+      {/* <TextToSpeech /> */}
+      </BrowserRouter>
+
       <Home />
     </main>
   );
